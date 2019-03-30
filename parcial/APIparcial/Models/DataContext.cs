56c6071479424bc67e57b0d3d17f4f7b11987cbe
Samuel Peña <samuel_pena@hotmail.es>
2019-03-30 11:16:40 -0400

    commit con modelo web api

---
+++ parcial/APIparcial/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+//ctl punto eliminar
+namespace APIparcial.Models
+{
+    public class DataContext:DbContext  // using data entity solo el uno
+    {
+        public DataContext():base("DefaultConnection")//ctor tabtab   //y luego ir a webconfig para ver como se escribe  //
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<APIparcial.Models.Product> Products { get; set; }
+    }
+}
---
+++ parcial/APIparcial/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIparcial.Models
+{
+
+    public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+    }
+    public enum TipoUnidad //enum es un listado constante para crear un tipo de dato, son parte de la clase el estado no es tipo entero ni string sino unidad, es un nuevo dominio de dato
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }//prod tabtab
+
+        [Required]//contrl punto y using // si o si ingresaod como dato
+        [Display(Name = "Product Name")] //muestra en el backend el producto
+
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public TipoUnidad Unidad { get; set; }
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+
+    }
+}
